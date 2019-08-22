4d561fe00ac80a0e999628bf1982b7cd4671d469
cs4alhaider <cs.alhaider@gmail.com>
2019-08-22 23:13:13 +0300

    Initial Commit

---
+++ CourseTimeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CourseTimeline
+//
+//  Created by Abdullah Alhaider on 22/08/2019.
+//  Copyright © 2019 Abdullah Alhaider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
